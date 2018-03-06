7de9923a190326db0368ba312ab15e09174b8a51
Dan Sasaki <dansasaki@Dans-MacBook-Pro.local>
2018-03-05 20:06:38 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -26,7 +26,8 @@ $(document).ready(function() {
 					loadQuestion(index);
 				} else {
 					$(".trueFalse").hide();
-					showScore();
+                    showScore();
+                    
 				}
 			}
 		}
